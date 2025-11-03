bf72392e4041fb129610972aec5ba0959de7ec53
Chintu <bharathchukka@gmail.com>
2025-11-03 15:53:49 +0530

    optimized and added gitignore

--- email_automation/utils/scheduler.py
+++ email_automation/utils/scheduler.py
@@ -3,16 +3,6 @@ import time
 from utils.email_utils import Email_Automation
 from datetime import datetime
 
-# def task_scheduler():
-#     print('sending a mail for every 10 sec....')
-#     schedule.every().seconds.do(Email_Automation().send_email)
-
-#     while True:
-#         schedule.run_pending()
-#         time.sleep(2)
-
-
-
 def task_scheduler():
     if datetime.now().day == 1 and datetime.now().hour == 10:
         print('sending a mail for every 1st day of month at 10 AM....')
@@ -20,4 +10,4 @@ def task_scheduler():
        
         while True:
             schedule.run_pending()
-            time.sleep(60)  # Check every minute
+            time.sleep(60)
